ca2810527aff415e0dbf8bab8710d64f8fbffbe2
Daniel Molnar <dm@ikusei.de>
2015-06-22 02:52:49 +0200

    reactjs integriert

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,19 +2,19 @@ class WelcomeController < ApplicationController
   def index
     # store credentials in yml and .gitignore the file?? or database??
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "xxx"
-      config.consumer_secret     = "xxx"
-      config.access_token        = "xxx"
-      config.access_token_secret = "xxx"
+      config.consumer_key        = Rails.application.secrets.twitter["consumer_key"]
+      config.consumer_secret     = Rails.application.secrets.twitter["consumer_secret"]
+      config.access_token        = Rails.application.secrets.twitter["access_token"]
+      config.access_token_secret = Rails.application.secrets.twitter["access_token_secret"]
     end
 
-    text = client.search("#hurricane #southside").take(20).collect do |tweet|
+    texts = client.search("#hurricane15 #southside15").collect do |tweet|
       tweet.text
     end
 
-    response = HTTParty.get("https://api.vineapp.com/tags/search/love?page=4")
-    text = response
+    # response = HTTParty.get("https://api.vineapp.com/tags/search/love?page=4")
+    # text = response
 
-    render json: text
+    @texts = texts
   end
 end
