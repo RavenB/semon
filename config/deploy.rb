acc6480bde4b76ef55e3440be82dcc4e48b1be58
Holger Frohloff <holger@5minutenpause.com>
2015-07-01 18:43:05 +0200

    Versucht die Assets zu deployen

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :linked_files, %w{config/database.yml config/unicorn.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/assets}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/assets}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
